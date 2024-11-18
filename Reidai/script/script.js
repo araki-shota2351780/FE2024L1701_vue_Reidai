73f20c78ded63280fcb38d941f8298bfe72fb4dc
araki-shota2351780 <araki.shota0227@gmail.com>
2024-11-18 01:20:50 +0000

    aa

--- Reidai/script/script.js
+++ Reidai/script/script.js
@@ -1,16 +1,16 @@
-// JavaScript用ファイル
 new Vue({
-    el: '#app',
-    
-    data: {
-    },
-    computed: {
+  el: '#app',
+  data: {
+      password: '',
+      passwordConfirm: ''
+  },
+  computed: {
       // 入力されている、且つパスワードが一致しているかを確認する算出プロパティ
-      // 入力されているかは、this.･･･ で判定可能
+      // 入力されているかは、this.password と this.passwordConfirm で判定可能
       // 例）一致TRUE（パスワード：aaaa、パスワード確認：aaaa）
       // 例）不一致FALSE（パスワード：aaaa、パスワード確認：aaab）
-
-      // ボタンをTRUEなら登録を表示orFALSEなら使用不可を表示にする算出プロパティ
-
-    }
-  });
+      isPasswordValid() {
+          return this.password && this.passwordConfirm && this.password === this.passwordConfirm;
+      }
+  }
+});
